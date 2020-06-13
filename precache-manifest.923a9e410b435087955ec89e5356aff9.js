015c6ce25040c2bfa37f2aac30c6ae2b5b522389
evan <yu.byoungmo@gmail.com>
2020-06-13 12:14:06 -0600

    Updates

---
+++ precache-manifest.923a9e410b435087955ec89e5356aff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a39702bafd40e7737f7aa76e27060f7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "17ec907f8b73332a5bc2",
+    "url": "/movie_app_2020/static/css/main.af1b5897.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ec907f8b73332a5bc2",
+    "url": "/movie_app_2020/static/js/main.fa5c0c47.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
